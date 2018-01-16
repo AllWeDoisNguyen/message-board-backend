b7e896d3a9fba957eca6e25c148d35c4f73fcd24
Christina Nguyen <cnguyen2@sapient.com>
2018-01-16 14:20:54 -0500

    created a post service and now we are able to receive a JSON object

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 
 var messages = [{text: 'some text', owner: 'Bob'},{text: 'other message', owner: 'Janie'}];
+
+app.use(bodyParser.json()); // body-parser's middleware
 
 app.use((req, res, next) => { // CORS because the ports are different URLS
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,6 +19,11 @@ app.get('/', (req, res) => {
 
 app.get('/messages', (req, res) => {
   res.json(messages);
+})
+
+app.post('/message', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
 })
 
 // app.get('/api/messages', (req, res) => { you could do this instead of the api.js and messages-mock
